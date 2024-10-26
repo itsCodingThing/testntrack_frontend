ad07d1816c9d9427a532f4840c5c78e4032fe66a
bhanu pratap singh <bhanuparitapsingh09@gmail.com>
2024-10-26 17:48:29 +0000

    refactor backend api index file

---
+++ src/lib/backend-apis/index.ts
@@ -0,0 +1,11 @@
+import * as admin from "./admin";
+import * as school from "./school";
+import * as login from "./login";
+
+const backendAPI = {
+  admin,
+  school,
+  login,
+};
+
+export default backendAPI;
